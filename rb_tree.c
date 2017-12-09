b1dfeb680da414d079243306001e64e2a94b59ed
Jason Ekstrand <jason.ekstrand@intel.com>
2017-12-09 12:50:29 -0800

    Add helpers to do rotations

--- rb_tree.c
+++ rb_tree.c
@@ -78,3 +78,58 @@ rb_node_set_parent(struct rb_node *n, struct rb_node *p)
 {
     n->parent = (n->parent & 1) | (uintptr_t)p;
 }
+
+/**
+ * Replace the subtree of T rooted at u with the subtree rooted at v
+ *
+ * This is called RB-transplant in CLRS.
+ *
+ * Both nodes are assumed to be non-leaves
+ */
+static void
+rb_tree_splice(struct rb_tree *T, struct rb_node *u, struct rb_node *v)
+{
+    RB_TREE_ASSERT(u);
+    RB_TREE_ASSERT(v);
+
+    struct rb_node *p = rb_node_parent(u);
+    if (p == NULL) {
+        RB_TREE_ASSERT(T->root == u);
+        T->root = v;
+    } else if (u == p->left) {
+        p->left = v;
+    } else {
+        p->right = v;
+    }
+    rb_node_set_parent(v, p);
+}
+
+static void
+rb_tree_rotate_left(struct rb_tree *T, struct rb_node *x)
+{
+    RB_TREE_ASSERT(x);
+    RB_TREE_ASSERT(x->right);
+
+    struct rb_node *y = x->right;
+    x->right = y->left;
+    if (y->left)
+        rb_node_set_parent(y->left, x);
+    rb_tree_splice(T, x, y);
+    y->left = x;
+    rb_node_set_parent(x, y);
+}
+
+static void
+rb_tree_rotate_right(struct rb_tree *T, struct rb_node *y)
+{
+    RB_TREE_ASSERT(y);
+    RB_TREE_ASSERT(y->left);
+
+    struct rb_node *x = y->left;
+    y->left = x->right;
+    if (x->right)
+        rb_node_set_parent(x->right, y);
+    rb_tree_splice(T, y, x);
+    x->right = y;
+    rb_node_set_parent(y, x);
+}
